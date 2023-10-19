cc4a17429de4a1fd3ea33917e2d624428086d4cf
Leiker Castillo <leikercg@gmail.com>
2023-10-19 18:34:59 +0200

    aaa

--- EntornoServidor/tema8_ejer1/control.php
+++ EntornoServidor/tema8_ejer1/control.php
@@ -4,13 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="estilos.css">
 </head>
 <body>
 <?php
 $nombre = $_REQUEST["nombre"];
- echo "$nombre";
+$apellidos = $_REQUEST["apellidos"];
+$anio = $_REQUEST["anio"];
+$animal = $_REQUEST["animal"];
 
-print_r ($_GET);
+echo "<p>Bienvenido $nombre $apellidos<p>";
+
+if($anio==2023){
+    echo "<p>Respuesta 1 correcta <p>";
+}else{
+    echo "<p>Respuesta 1 incorrecta <p>";
+}
+
     ?>
 </body>
 </html>
