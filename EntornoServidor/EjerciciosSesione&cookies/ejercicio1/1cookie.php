0fa638f346e92eb8c8014cdf7b0421c2f2b1d551
leikercg <leikercg@gmail.com>
2023-11-17 20:28:49 +0100

    viernes 17/11

---
+++ EntornoServidor/EjerciciosSesione&cookies/ejercicio1/1cookie.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=re, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+   <header>
+    <h1>USO DE COOKIES</h1>
+   </header>
+   <main>
+
+
+    <h2>Inicio de sesión</h2>
+    <form action="2cookie.php">
+        <label>Escribe tu nombre</label>
+        <input type="text" name="nombre" id=""><br>
+        <label>Color: </label>
+        <select name="color" id="">
+            <option value="red">rojo</option>
+            <option value="blue">azul</option>
+            <option value="green">verde</option>
+        </select>
+        <input type="submit" value="enviar" id="">
+    </form>
+   </main>
+
+</body>
+</html>
