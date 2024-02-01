7252f0e10bbc5433807b98827c3e1cf0b1813806
leikercg <leikercg@gmail.com>
2024-02-01 17:14:05 +0100

    inicio Laravel

---
+++ Laravel/example-app/app/Http/Controllers/HolaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HolaController extends Controller
+{
+    //
+    public function __invoke()
+    {
+        return "Hola, mundo";
+    }
+    public function show($nombre) {
+        $data['nombre'] = $nombre;
+        return view('hola', $data);
+    }
+
+
+}
