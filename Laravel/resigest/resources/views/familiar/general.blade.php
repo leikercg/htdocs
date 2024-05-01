a10ead780020c33413ba2331ca6442dfb3496a3e
leikercg <leikercg@gmail.com>
2024-05-01 17:38:29 +0200

    traduccion de paginas de familiares

--- Laravel/resigest/resources/views/familiar/general.blade.php
+++ Laravel/resigest/resources/views/familiar/general.blade.php
@@ -2,34 +2,43 @@
 @section('title', 'Familiares')
 
 @section('content')
+<div class="row">
+    <nav style="--bs-breadcrumb-divider: '>';" aria-label="{{ __('breadcrumb') }}">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="">{{ __('Lista de residentes') }}</a></li>
+        </ol>
+    </nav>
+</div>
     <div class="row">
         <div class="col-12 text-center">
-            <h2>{{__('mensaje.lista')}}</h2>
+            <h2>{{ __('LISTA DE RESIDENTES') }}</h2>
         </div>
     </div>
 
     <div class="row justify-content-center">
-        <div class="col-md-6">
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Apellidos</th>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Edad</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($residentes as $residente)
-                        <tr onclick="window.location='{{ route('ficha.residente', $residente->id) }}';">
-                            <td>{{ $residente->id }}</td>
-                            <td>{{ $residente->apellidos }}</td>
-                            <td>{{ $residente->nombre }}</td>
-                            <td>{{ $residente->edad }}</td>
+        <div class="row justify-content-center">
+            <div class="col-10 col-sm-8">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">{{ __('ID') }}</th>
+                            <th scope="col">{{ __('Apellidos') }}</th>
+                            <th scope="col">{{ __('Nombre') }}</th>
+                            <th scope="col">{{ __('Edad') }}</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach ($residentes as $residente)
+                            <tr onclick="window.location='{{ route('ficha.residente', $residente->id) }}';">
+                                <td>{{ $residente->id }}</td>
+                                <td>{{ $residente->apellidos }}</td>
+                                <td>{{ $residente->nombre }}</td>
+                                <td>{{ $residente->edad }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
