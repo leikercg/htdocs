72e33f95111f95f627c5b49dcc7e3cdec7546ac0
leikercg <leikercg@gmail.com>
2024-05-15 20:36:31 +0200

    cambiar favicon en las páginas creadas por defecto acabado

--- Laravel/resigest/resources/views/welcome.blade.php
+++ Laravel/resigest/resources/views/welcome.blade.php
@@ -4,11 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>ResiGest</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/png" href="{{ asset('images/icono.png') }}" /> {{--Fav ico --}}
+
 
         <!-- Styles -->
         <style>
